1bde115451c7b7bb75ccba2365705d68331f99ba
SpookyNoodlez <55265731+SpookyNoodlez@users.noreply.github.com>
2022-03-02 19:45:29 +0200

    Tom sträng testad

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,2 +1,17 @@
 public class StringCalculator {
+
+    public int Add(String numbers){
+        int  result = 0;
+        if (numbers == ""){
+            return result;
+        }
+
+        String[] tokens = numbers.split(",");
+
+        for(int i = 0; i<3; i++){
+            result += Integer.parseInt(tokens[i]);
+        }
+
+        return result;
+    }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringCalculatorTest {
+    @Test
+    void testAddOnEmptyReturnsZero(){
+        assertEquals(0, new StringCalculator().Add(""));
+    }
 }
